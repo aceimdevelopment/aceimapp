7518988eebbcddc1073be9f5b5e693c7df681f82
daniel <moros.daniel@gmail.com>
2018-05-03 23:05:19 -0400

    Ajuste

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,15 @@ module Aceimapp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.i18n.default_locale = :es
+
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    config.assets.paths << Rails.root.join("assets", "fonts")
+
+
   end
 end
