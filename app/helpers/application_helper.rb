5ce2ce27bcb730d16a929c8d08b03a742cd92393
daniel <moros.daniel@gmail.com>
2018-05-06 19:35:04 -0400

    Carga de la cartelera 3h

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
 
 	end
 
-	def card_to_info tipo_inscripcion, dias, fecha_inscripcion='Por definir', fecha_planilla='Por definir', arancel='Por definir' 
+	def card_to_info tipo_inscripcion, dias, apertura=nil, cierre=nil, apertura_planilla=nil, cierre_planilla=nil, arancel='Por definir' 
 
 		capture_haml do
 			haml_tag :div, {class: 'card'} do
@@ -18,19 +18,59 @@ module ApplicationHelper
 				haml_tag :div, {class: 'card-body'} do
 					haml_tag :ul do
 						haml_tag :li do
-							info 'Fecha y Hora de Inscripción:', fecha_inscripcion 
+							if apertura.nil?
+								info 'Fecha y Hora de Inscripción:', 'Por definir'
+							else
+								info 'Fecha y Hora de Inscripción:', (formato_fecha_inscripcion apertura, cierre).to_s
+							end
 						end
 						haml_tag :li do
-							info 'Fecha y Hora de Entrega de Planilla:', fecha_planilla
+
+							if apertura_planilla.nil?
+								info 'Fecha y Hora de Entrega de Planilla:', 'Por definir'
+							else
+								info 'Fecha y Hora de Entrega de Planilla:', (formato_fecha_entrega_planilla apertura_planilla, cierre_planilla).to_s
+							end
 						end
 						haml_tag :li do
-							info 'Arancel por nivel:', fecha_planilla
+							info 'Arancel por nivel:', arancel
 						end
 					end
 				end
 			end
 		end
 	end
+
+
+
+	def formato_fecha_inscripcion apertura, cierre
+		fecha_formato apertura, cierre
+	end
+
+	def formato_fecha_entrega_planilla apertura, cierre
+		fecha_formato apertura, cierre
+	end
+
+	def fecha_formato (fecha_inicial, fecha_final)
+		if fecha_inicial.blank? or fecha_final.blank?
+			return "Por definir"
+		else
+			if fecha_inicial.to_date.eql? fecha_final.to_date		
+				fecha = I18n.localize(fecha_inicial, :format => "El %A, %d de %B de %Y ")
+				if (fecha_inicial.hour < 12) and (fecha_final.hour > 12)
+					fecha += I18n.localize(fecha_inicial, :format => "desde las %I:%M %p hasta las 12:00 M y desde las 2:00 PM hasta las ")
+					fecha += I18n.localize(fecha_final, :format => "%I:%M %p")
+				else
+					fecha += I18n.localize(fecha_inicial, :format => "desde las %I:%M %p hasta las ")
+					fecha += I18n.localize(fecha_final, :format => "%I:%M %p")					
+				end	
+			else
+				fecha = "Desde el #{fecha_apertura} hasta el #{fecha_cierre}"
+			end			
+			return fecha
+		end
+	end
+
 
 
 end
